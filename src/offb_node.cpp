e9a76fa84772b1e40602457e8b538b23902c188a
minwoo98 <cola31929395@gmail.com>
2022-07-05 15:55:21 +0900

    attitude+thrust control backup

--- src/offb_node.cpp
+++ src/offb_node.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Quaternion.h>
 #include <geometry_msgs/TwistStamped.h>
 #include <nav_msgs/Odometry.h>
 #include <mavros_msgs/CommandBool.h>
@@ -7,22 +8,39 @@
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
 #include <tf/transform_datatypes.h>
+#include <tf/transform_broadcaster.h>
+#include <sensor_msgs/Imu.h>
 
 //----this code should be written in theh header file later----//
 mavros_msgs::State current_state;
 nav_msgs::Odometry odom;
 
+double roll, pitch, yaw;
+
 int set_goal_cnt = 0;
 int count = 0;
+
+double vth;
+double goal_yaw;
 
 double pre_err_lin_x;
 double pre_err_lin_y;
 double pre_err_lin_z;
 
+double pre_err_roll;
+double pre_err_pitch;
+double pre_err_yaw;
 double pre_err_thrust;
 
 const double kp_lin_vel = 0.45;
 const double kd_lin_vel = 0.15;
+
+const double kp_roll_dist = 0.15;
+const double kp_pitch_dist = 0.3;
+const double kp_yaw_dist = 0.3;
+const double kp_roll = 0.3;
+const double kp_pitch = 0.3;
+const double kp_yaw = 0.3;
 
 const double kp_thrust = 0.33; // 0.35 down
 const double kd_thrust = 1.2;
@@ -33,6 +51,11 @@ double dist(nav_msgs::Odometry now_pose, geometry_msgs::PoseStamped goal);
 
 void state_cb(const mavros_msgs::State::ConstPtr& msg){
     current_state = *msg;
+}
+void imu_cb(const sensor_msgs::Imu::ConstPtr& msg)
+{
+    vth = msg->angular_velocity.z;
+    //ROS_INFO("%.3f", vth);
 }
 void odom_cb(const nav_msgs::Odometry::ConstPtr& msg){
 
@@ -48,10 +71,8 @@ void odom_cb(const nav_msgs::Odometry::ConstPtr& msg){
     tf::Quaternion q(odom.pose.pose.orientation.x, odom.pose.pose.orientation.y, odom.pose.pose.orientation.z, odom.pose.pose.orientation.w);
     tf::Matrix3x3 mat(q);
 
-    double roll, pitch, yaw;
     mat.getRPY(roll,pitch,yaw);
-
-    ROS_INFO("%.2f | %.2f | %.2f ", roll,pitch,yaw);
+    //ROS_INFO("%.2f | %.2f | %.2f ", roll,pitch,yaw);
 }
 double dist(nav_msgs::Odometry now, geometry_msgs::PoseStamped goal)
 {
@@ -63,29 +84,46 @@ double dist(nav_msgs::Odometry now, geometry_msgs::PoseStamped goal)
 
     return dist;
 }
+geometry_msgs::Quaternion rpy_to_quat(double roll, double pitch, double yaw)
+{
+    tf::Quaternion q;
+    geometry_msgs::Quaternion att_quat;
+
+    q.setRPY(roll,pitch,yaw);
+    tf::quaternionTFToMsg(q,att_quat);
+
+    att_quat.x *= -1;
+    att_quat.y *= -1;
+    att_quat.z *= -1;
+    att_quat.w *= -1;
+
+    /*
+    ROS_INFO("ORI %.2f | %.2f | %.2f | %.2f ", odom.pose.pose.orientation.x, odom.pose.pose.orientation.y,odom.pose.pose.orientation.z,odom.pose.pose.orientation.w);
+    ROS_INFO("ORI %.2f | %.2f | %.2f | %.2f ", att_quat.x,att_quat.y,att_quat.z,att_quat.w);
+    ROS_INFO("----------------------");
+    */
+    return att_quat;
+
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "offb_node");
     ros::NodeHandle nh;
 
     //subscriber
-    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
-            ("mavros/state", 10, state_cb);
-    ros::Subscriber odom_sub = nh.subscribe<nav_msgs::Odometry>
-            ("mavros/local_position/odom", 10, odom_cb);
+    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>("mavros/state", 10, state_cb);
+    ros::Subscriber odom_sub = nh.subscribe<nav_msgs::Odometry>("mavros/local_position/odom", 10, odom_cb);
+    ros::Subscriber imu_sub = nh.subscribe<sensor_msgs::Imu>("mavros/imu/data", 20, imu_cb);
 
     //publisher
-    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
-            ("mavros/setpoint_position/local", 10);
-    ros::Publisher local_vel_pub = nh.advertise<geometry_msgs::TwistStamped>
-            ("mavros/setpoint_velocity/cmd_vel", 10);
-    ros::Publisher att_pub = nh.advertise<mavros_msgs::AttitudeTarget>
-            ("mavros/setpoint_raw/attitude", 10);
+    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 10);
+    ros::Publisher local_vel_pub = nh.advertise<geometry_msgs::TwistStamped>("mavros/setpoint_velocity/cmd_vel", 10);
+    ros::Publisher att_pub = nh.advertise<mavros_msgs::AttitudeTarget>("mavros/setpoint_raw/attitude", 10);
+
     //serviceclient
-    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
-            ("mavros/cmd/arming");
-    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
-            ("mavros/set_mode");
+    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
+    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
 
     //the setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(20.0);
@@ -118,6 +156,7 @@ int main(int argc, char **argv)
     arm_cmd.request.value = true;
 
     ros::Time last_request = ros::Time::now();
+    //
     ros::Time current_time, last_time;
     current_time = ros::Time::now();
     last_time = ros::Time::now();
@@ -145,9 +184,8 @@ int main(int argc, char **argv)
         current_time = ros::Time::now();
         double dt = (current_time - last_time).toSec();
 
-        
         //postition control - square path
-        #if 1
+        #if 0
         if(dist(odom, goal_pose) < 0.18)
         {
             //ROS_INFO("arrived at [%d]", set_goal_cnt);
@@ -183,6 +221,7 @@ int main(int argc, char **argv)
         }
     
         local_pos_pub.publish(goal_pose); 
+
         #endif
 
         //velocity_control - circle path
@@ -227,25 +266,27 @@ int main(int argc, char **argv)
         #endif
 
         //attitude + thrust control - square path
-        #if 0
+        #if 1
+
         if(dist(odom, goal_pose) < 0.18)
         {
             //ROS_INFO("arrived at [%d]", set_goal_cnt);
             if(set_goal_cnt == 0)
+            {
+                
+                goal_pose.pose.position.x = 0;
+                goal_pose.pose.position.y = 4;
+                goal_pose.pose.position.z = 4;
+                set_goal_cnt = 1;
+            }
+            else if(set_goal_cnt == 1)
             {
                 goal_pose.pose.position.x = 0;
                 goal_pose.pose.position.y = 0;
                 goal_pose.pose.position.z = 4;
                 set_goal_cnt = 0;
             }
-            else if(set_goal_cnt == 1)
-            {
-                goal_pose.pose.position.x = 4;
-                goal_pose.pose.position.y = 4;
-                goal_pose.pose.position.z = 4;
-                set_goal_cnt = 2;
-            }
-            
+            /*
             else if(set_goal_cnt == 2)
             {
                 goal_pose.pose.position.x = 4;
@@ -260,8 +301,47 @@ int main(int argc, char **argv)
                 goal_pose.pose.position.z = 4;
                 set_goal_cnt = 0;
             }
+            */
         }
-        /*
+
+        //local_pos_pub.publish(goal_pose);  //
+
+        
+        double target_roll;
+        double offset_dist_y = goal_pose.pose.position.y - odom.pose.pose.position.y;
+        double err_roll;
+        double roll_output;
+        geometry_msgs::Quaternion att_quat;
+        
+        //남은 거리에 따른 목표roll 설정
+        if(offset_dist_y > 0)   target_roll = (-1)* kp_roll_dist * offset_dist_y; //원점->y축방향 이동
+        else if(offset_dist_y < 0)   target_roll = kp_roll_dist * offset_dist_y; //y축->원점방향 이동
+
+        err_roll = target_roll - roll;
+        
+        //ROS_INFO("tar : %.2f | now: %.2f ", target_roll, roll);
+        roll_output = kp_roll*err_roll;
+        att_quat = rpy_to_quat(roll,pitch,yaw);
+
+        goal_att.type_mask = 7;
+        goal_att.thrust = 0.85;
+        goal_att.orientation = att_quat;
+
+        att_pub.publish(goal_att);
+
+
+        
+
+
+
+
+
+        //double target_pitch = kp_pitch_dist*(goal_pose.pose.position.x - odom.pose.pose.position.x);
+        //double tartget_yaw = kp_yaw_dist*(90 - odom.pose.pose.orientation.w);
+
+   
+        /* 
+        <thrust control>
         //goal_att.thrust = 0.8;
         double err_thrust = goal_pose.pose.position.z - odom.pose.pose.position.z;
         goal_att.thrust = kp_thrust*err_thrust + kd_thrust*(err_thrust - pre_err_thrust)*dt;
